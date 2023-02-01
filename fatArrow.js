7db484a3459922127c6ec8263fbfe5f8c1dd4686
RichithaaReddy <richithareddie@gmail.com>
2023-02-01 11:33:03 +0530

    day 1

---
+++ fatArrow.js
@@ -0,0 +1,33 @@
+//function [basic] o/p =[Function: hello]
+// const sum = function hello(){
+//     return 2+3;
+// }
+// console.log(sum);
+
+
+
+//fat arrow o/p=5
+// const sum = () => 2+3;
+// console.log(sum());
+
+
+
+//with 2 parameters
+// const sum = (a,b) => a+b;
+// console.log(sum(1,3));
+
+
+
+//with 1 parameters
+const sum = (a) => a;
+console.log(1);
+
+
+
+//json format
+const student={
+    name: "richi",
+    rno:  1233
+};
+console.log(student);  // prints student obj
+console.log(student.name); // prints name
