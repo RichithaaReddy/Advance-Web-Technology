b5996d92b88a99eeca3dd5dceb3572bb5b190e81
richitha <richithareddie@gmail.com>
2023-02-12 12:25:00 +0530

    added

---
+++ e.js
@@ -0,0 +1,10 @@
+const message = () => {
+    const name = "Jesse";
+    const age = 40;
+    return{name,age};
+  };
+  
+  export default message;
+
+
+  //modules cant be used without json file in the node envrmnt
---
+++ i.js
@@ -0,0 +1,6 @@
+import message from "./e.js"
+
+console.log(message());
+
+
+//modules cant be used without json file in the node envrmnt
